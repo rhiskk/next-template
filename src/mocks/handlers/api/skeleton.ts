97b0d39d2506ca23c698502c1facec64d17b55c7
rhiskk <hiski.r@gmail.com>
2023-02-15 20:39:16 +0200

    thoughts

--- src/mocks/handlers/api/skeleton.ts
+++ src/mocks/handlers/api/skeleton.ts
@@ -1,6 +1,9 @@
 import { mswTrpc } from "../../mswTrpc";
 
 export const skeletonHandlers = [
+  /*
+    msw-trpc has some problems with ctx typings apparently
+  */
   mswTrpc.skeleton.hello.query((req, res, ctx) => {
     return res(
       ctx.status(200),
--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,9 +1,23 @@
 import Home from "..";
 
-describe("Greeting", () => {
-  // it("renders", () => {
-  //   render(<Home />);
-  // });
+describe("Index", () => {
+  /*
+  Cannot render <Home/> because it needs a trpc provider wrapped around it 
+  which needs a client that cannot run in node as far as I know.
+  Gotta find a way around this.
+  */
+  /*
+  Tried to set up like this.
+  it("renders", () => {
+    render(
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <Home />
+        </QueryClientProvider>
+      </trpc.Provider>
+    );
+  });
+  */
   it("renders", () => {
     expect(1).toBe(1);
   });
